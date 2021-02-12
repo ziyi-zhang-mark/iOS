fd8334d14712f1a092cdc61f87afb1da7a9ed552
Ziyi Zhang <zhangziyibupt@gmail.com>
2021-02-12 11:04:27 -0800

    Struct & MVC design pattern

--- swift/Dicee/Dicee-iOS13/ViewController.swift
+++ swift/Dicee/Dicee-iOS13/ViewController.swift
@@ -18,5 +18,10 @@ class ViewController: UIViewController {
         let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
         diceImageView1.image = diceArray[Int.random(in: 0...5)]
         diceImageView2.image = diceArray.randomElement()
+        
+        let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+        let hardness = sender.currentTitle!
+        let result = eggTimes[hardness]!  // type of result is Int
+        print(result)
     }
 }
